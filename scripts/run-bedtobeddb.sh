c7f5444cf0add36cf55668425328dbba62542f08
Lmercadom <42242797+Lmercadom@users.noreply.github.com>
2018-11-16 11:37:01 -0500

    Update run-bedtobeddb.sh

--- scripts/run-bedtobeddb.sh
+++ scripts/run-bedtobeddb.sh
@@ -4,7 +4,27 @@
 
 INPUT=$1
 ASSEMBLY=$2
+OUTDIR=$3
+
+FILE_BASE=$(basename $INPUT)
+FILE_NAME=${FILE_BASE%.*}
+echo $FILE_NAME
+
+mkfifo pp
+
+if [ ! -d "$OUTDIR" ]
+then
+  mkdir $OUTDIR
+fi
+
+gunzip -c $INPUT > pp.bed
+
+outputfile="$OUTDIR/$FILE_NAME.beddb"
+echo $outputfile
 
 clodius aggregate bedfile \
-    --assembly $ASSEMBLY \
-    $INPUT
+  --assembly $ASSEMBLY -o $outputfile \
+   pp.bed
+
+rm pp
+rm pp.bed
